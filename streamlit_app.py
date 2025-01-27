4483e30cda8e7eff90ca0faa8cdefb2088e163ef
Oscar Cordero Boronat <105495781+Oscar-Cordero-Boronat@users.noreply.github.com>
2025-01-27 13:06:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My news app")
+st.title("🎈 My newssssssssssssssssssssss app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
